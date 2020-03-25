f420c4cf7d34b04545c904e0d8d8e73c5170f104
Perrine <perrine.brianchon@gmail.com>
2020-03-25 16:24:06 +0100

    add nav

--- jva/src/components/Button.js
+++ jva/src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 
 const TestButton = () => {
-    return <Button variant="outline-secondary">Secondary</Button>
+    return <Button variant="outline-secondary">Ceci est un composant</Button>
 }
 
 export default TestButton;
