78615602006b80a9bce011d28bd512faef2caeb1
Perrine <perrine.brianchon@gmail.com>
2020-03-25 15:14:44 +0100

    install & test bootstrap

---
+++ jva/src/components/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+const TestButton = () => {
+    return <Button variant="outline-secondary">Secondary</Button>
+}
+
+export default TestButton;
