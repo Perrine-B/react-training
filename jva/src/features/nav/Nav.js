0c6cc44352cd55222291a1a6f6015a1561e66127
Perrine <perrine.brianchon@gmail.com>
2020-03-27 17:33:06 +0100

    install react-router

--- jva/src/features/nav/Nav.js
+++ jva/src/features/nav/Nav.js
@@ -2,9 +2,7 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
-import Button from 'react-bootstrap/Button';
+
 
 export default function TestNav() {
     return (
@@ -22,10 +20,6 @@ export default function TestNav() {
               <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
             </NavDropdown>
           </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Chercher un truc" className="mr-sm-2" />
-            <Button variant="outline-success">Rechercher</Button>
-          </Form>
         </Navbar.Collapse>
       </Navbar>
     )}
